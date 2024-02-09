cc74a90813f933ca5c16a81540b9af95e67aee42
Kulagin Yuri <jkulvichi@gmail.com>
2024-02-09 17:05:41 +0300

    fix: gh-pages base url fix
    Signed-off-by: Kulagin Yuri <jkulvichi@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { fileURLToPath, URL } from 'node:url'
+import { URL, fileURLToPath } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const BASE_URL = process.env.BASE_URL || './'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: BASE_URL,
   plugins: [
     vue(),
   ],
